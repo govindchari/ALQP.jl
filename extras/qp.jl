3a440c95a1163d40322a51d3923b83754a91846c
Govind Chari <govindchari1@gmail.com>
2022-04-20 15:43:37 -0400

    Started working

--- extras/qp.jl
+++ extras/qp.jl
@@ -1,5 +1,4 @@
 using LinearAlgebra
-using SparseArrays
 
 struct QP
     # problem data
@@ -11,30 +10,41 @@ struct QP
     d::Array{Float64,1}
     AAt::Array{Float64,2}
 
+    ∇f::Array{Float64,1}
+    ∇2f::Array{Float64,2}
+    α::Float64
+   
     x::Array{Float64,1}
+    Δx::Array{Float64,1}
     λ::Array{Float64,1}
     μ::Array{Float64,1}
     ρ::Float64
+    ϕ::Float64
 
     function QP(Q,q,A,b,C,d)
         AAt = A*A'
         x = zeros(length(q))
         λ = zeros(length(b))
         μ = zeros(length(d))
-        new(Q,q,A,b,C,d,AAt,x,λ,μ)
+        ∇f = zeros(length(q))
+        ∇2f = zeros(length(q),length(q))
+        ϕ = 10.0
+        new(Q,q,A,b,C,d,AAt,∇f,∇2f,x,λ,μ,ϕ)
     end
 end
 function update_dual!(qp::QP)
-    
+    λ .= λ .+ ρ*(A*x-b)
+    μ .= max.(0,μ+ρ*(C*x-d))
 end
 function update_penalty!(qp::QP)
-    
+    ρ = ρ*ϕ
 end
 function newton_step!(qp::QP)
-
+    Δx .= -∇2f\∇f
+    x .= x.+Δx
 end
 function linesearch!(qp::QP)
-    
+
 end
 function minimize_augmented_lagrangian!(qp::QP)
     
