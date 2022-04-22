44e053dd696763873e88833158b1a07b3bd76270
Govind Chari <govindchari1@gmail.com>
2022-04-22 18:12:39 -0400

    Benchmarking

--- extras/qp.jl
+++ extras/qp.jl
@@ -101,6 +101,7 @@ function solve!(qp::QP,verbose::Bool)
 end
 
 let
+    #=
     n = 2
     Q = [1 0;0 1]
     q = zeros(n)
@@ -115,6 +116,26 @@ let
     C = sparse(C)
 
     qp = QP(Q,q,A,b,C,d)
+    @btime solve!($qp)
+    =#
+    n = 10
+    Q = randn(n,n);Q = Q'*Q
+    Q = sparse(Q)
+    q = zeros(n)
+    A = spzeros(0,n)
+    b = []
+    G = sparse([I(n);-I(n)])
+    h = [ones(n);zeros(n)]
+
+    qp = QP(Q,q,A,b,G,h)
+    m = OSQP.Model()
+    OSQP.setup!(m; P = Q, q=q, A=sparse(I(10)), l=zeros(n), u=ones(n), verbose = false)
+
+    #Benchmarking
+    println("OSQP Benchmark:")
+    @btime OSQP.solve!($m)
+
+    println("ALQP Benchmark:")
     @btime solve!($qp)
 
 end
