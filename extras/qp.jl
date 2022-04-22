183b37aca9ef29d7374053c1e25dd857e561987c
Govind Chari <govindchari1@gmail.com>
2022-04-22 14:19:55 -0400

    First cut

--- extras/qp.jl
+++ extras/qp.jl
@@ -11,56 +11,81 @@ struct QP
 
     ∇L::Array{Float64,1}
     ∇2L::Array{Float64,2}
-    α::Float64
+    Iρ::Array{Float64,2}
 
     x::Array{Float64,1}
-    Δx::Array{Float64,1}
     λ::Array{Float64,1}
     μ::Array{Float64,1}
     ρ::Float64
     ϕ::Float64
 
+    aug_lag_tol::Float64
+
     function QP(Q, q, A, b, C, d)
-        AAt = A * A'
         x = zeros(length(q))
         λ = zeros(length(b))
         μ = zeros(length(d))
-        ∇f = zeros(length(q))
-        ∇2f = zeros(length(q), length(q))
+        ρ = 1
+        Iρ = I(length(d))
+        ∇L = zeros(length(q))
+        ∇2L = zeros(length(q), length(q))
         ϕ = 10.0
-        new(Q, q, A, b, C, d, AAt, ∇f, ∇2f, x, λ, μ, ϕ)
+        aug_lag_tol = 1e-6
+        new(Q, q, A, b, C, d, ∇L, ∇2L, Iρ, x, λ, μ, ρ, ϕ, aug_lag_tol)
     end
 end
 function update_dual!(qp::QP)
-    λ .= λ .+ ρ * (A * x - b)
-    μ .= max.(0, μ + ρ * (C * x - d))
+    qp.λ .= qp.λ .+ qp.ρ * (qp.A * qp.x - qp.b)
+    qp.μ .= max.(0, qp.μ + qp.ρ * (qp.C * qp.x - qp.d))
 end
 function update_penalty!(qp::QP)
-    ρ = ρ * ϕ
+    qp.ρ = qp.ρ * qp.ϕ
 end
 function newton_step!(qp::QP)
-    x .= x .- ∇2L \ ∇L
+    qp.x .= qp.x .- qp.∇2L \ qp.∇L
 end
-function linesearch!(qp::QP)
-
+function update_Iρ!(qp::QP)
+    qp.Iρ .= qp.ρ * Diagonal(qp.C * qp.x - qp.d .> zeros(length(qp.d)))
+end
+function update_derivatives!(qp::QP)
+    qp.∇L .= qp.Q * qp.x + qp.q + qp.A' * (qp.λ + qp.ρ * (qp.A * qp.x - qp.b)) + qp.C' * (qp.μ + qp.Iρ * (qp.C * qp.x - qp.d))
+    qp.∇2L .= qp.Q + qp.ρ * qp.A * qp.A' + qp.C' * qp.Iρ * qp.C
 end
 function minimize_augmented_lagrangian!(qp::QP)
-    update_I!()
-    while (tol)
-        update_derivatives!()
+    update_Iρ!(qp)
+    while (norm(qp.∇L) > qp.aug_lag_tol)
+        update_derivatives!(qp)
         newton_step!(qp)
+        print(qp.x)
     end
-
+    print(qp.∇L)
 end
 
 function solve!(qp::QP)
-    while minimize_augmented_lagrangian!(qp)
+    update_derivatives!(qp)
+    minimize_augmented_lagrangian!(qp)
+    #=
+    while (!converged)
+        minimize_augmented_lagrangian!(qp)
         update_dual!(qp)
         update_penalty!(qp)
         check_convergence!(qp)
     end
+    =#
 end
 
 let
+    n = 2
+    Q = randn(n,n);Q = Q'*Q
+    #Q = sparse(Q)
+    q = zeros(n)
+    Q = [1 0;0 2]
+    q = [1;1]
+    A = zeros(n,n)
+    b = zeros(n)
+    C = zeros(n,n)
+    d = zeros(n)
 
+    qp = QP(Q,q,A,b,C,d)
+    solve!(qp)
 end
